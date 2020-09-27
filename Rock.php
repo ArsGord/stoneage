a008f06c6d7247a569486205926f688b2d7f0ba1
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-09-27 20:57:14 +0400

    Delete Rock.php

--- Rock.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    class Rock extends Entity {
-        function __construct($data) {
-            parent::__construct($data);
-            $this->size = $data->size;
-            $this->count = $data->count;
-        }
-    }
