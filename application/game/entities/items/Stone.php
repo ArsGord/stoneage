308e1910df56586b3194079b0f89a6ac9c462327
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-09-28 10:38:24 +0400

    Update Stone.php

--- application/game/entities/items/Stone.php
+++ application/game/entities/items/Stone.php
@@ -6,5 +6,7 @@ class Stone extends Entity {
     function __construct($data) {
         parent::__construct($data);
         $this->count = $data->count;
+        $this->x = $data->x;
+        $this->y = $data->y;
     }
 }
