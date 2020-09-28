d7f455100bbd20a1785690761be78942946b18ab
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-09-28 10:38:04 +0400

    Update Wood.php

--- application/game/entities/items/Wood.php
+++ application/game/entities/items/Wood.php
@@ -6,5 +6,7 @@ class Wood extends Entity {
     function __construct($data) {
         parent::__construct($data);
         $this->count = $data->count;
+        $this->x = $data->x;
+        $this->y = $data->y;
     }
 }
