f8e2110c24ce1f31adb82f6871176de7884ff8c6
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-09-28 09:51:01 +0400

    Update Wood.php

--- application/game/entities/items/Wood.php
+++ application/game/entities/items/Wood.php
@@ -3,13 +3,5 @@
 class Wood extends Entity {
     function __construct($data) {
         parent::__construct($data);
-        $this->flinders = $data->flinders;
+        $this->count = $data->count;
     }
-
-    public function timber() {
-        if ($this->damage === 3) {
-            $this->flinders = 15;
-        }
-    }
-
-}
