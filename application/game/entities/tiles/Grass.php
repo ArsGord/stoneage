c98ed9aa8fd1f8ffafe9e71db3d3bb7c9511c2d4
NikBel3476 <nikita.belkin3476@gmail.ru>
2020-09-28 11:28:04 +0400

    plant and grass

--- application/game/entities/tiles/Grass.php
+++ application/game/entities/tiles/Grass.php
@@ -5,7 +5,7 @@ class Grass extends Entity
     public function __construct($data)
     {
         parent::__construct($data);
-        $this->stage = $data->stage; // стадия роста
+        $this->size = $data->size; // стадия роста
         $this->count = $$data->count; // пищевая ценность для животного
     }
 
