daed44d5c1d86ce2f18092abeac211d043d4baaf
AlexLuik001 <64197415+AlexLuik@users.noreply.github.com>
2020-09-28 09:51:52 +0400

    Update Tree.php

--- application/game/entities/tiles/Tree.php
+++ application/game/entities/tiles/Tree.php
@@ -3,7 +3,6 @@
     class Tree extends Entity {
         function __construct($data) {
             parent::__construct($data);
-            $this->size = $data->size;
             $this->type = $data->type;
             $this->count = $data->count;
         }
