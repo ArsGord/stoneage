652e7e994148d8e68be808f38809a6dff96d441d
NikBel3476 <63227044+NikBel3476@users.noreply.github.com>
2020-10-02 19:38:56 +0400

    cow and human fix

--- application/game/entities/units/Human.php
+++ application/game/entities/units/Human.php
@@ -14,8 +14,8 @@ class Human extends Animal {
         // если нанесен урон, то нанести его с учетом брони и одежды
         if ($damage > 0) {
             // учесть защиту игрока, вычесть часть дамаги из предметов
-            if($this->protection > 0){
-                $damage -= $this->protection;
+            if($this->body->protection){
+                $damage -= $this->body->protection;
             }
             // нанести оставшуюся дамагу с помощью
             return parent::hit($damage);
@@ -26,16 +26,24 @@ class Human extends Animal {
     protected function destroy() {
         // Все, что лежит в карманах, вывалить на карту (предметам задать x, y)
         // предмет из левой руки
-        $this->left_hand->x = $this->x;
-        $this->left_hand->y = $this->y;
+        if ($this->left_hand) {
+            $this->left_hand->x = $this->x;
+            $this->left_hand->y = $this->y;
+        }
         // предмет из правой руки
-        $this->right_hand->x = $this->x;
-        $this->right_hand->y = $this->y;
+        if ($this->right_hand) {
+            $this->right_hand->x = $this->x;
+            $this->right_hand->y = $this->y;
+        }
         // предмет из рюкзака
-        $this->backpack->x = $this->x;
-        $this->backpack->y = $this->y;
+        if ($this->backpack) {
+            $this->backpack->x = $this->x;
+            $this->backpack->y = $this->y;
+        }
         // надетая на тело одежда
-        $this->body->x = $this->x;
-        $this->body->y = $this->y;
+        if ($this->body) {
+            $this->body->x = $this->x;
+            $this->body->y = $this->y;
+        }
     }
 }
