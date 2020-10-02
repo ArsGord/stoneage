6718acebc9431c13d84e7253aef045c9197bb982
NikBel3476 <63227044+NikBel3476@users.noreply.github.com>
2020-10-02 19:46:03 +0400

    delete x and y from stone and wood

--- application/game/entities/items/Stone.php
+++ application/game/entities/items/Stone.php
@@ -4,7 +4,5 @@ class Stone extends Entity {
     function __construct($data) {
         parent::__construct($data);
         $this->count = $data->count; // количество камня
-        $this->x = $data->x;
-        $this->y = $data->y;
     }
 }
