4be88bec34a143bf4754d52a5ace75f91e2b52ca
Skovorodka-chan <67179020+Skovorodka-chan@users.noreply.github.com>
2020-10-02 19:17:16 +0400

    Update Wood.php

--- application/game/entities/items/Wood.php
+++ application/game/entities/items/Wood.php
@@ -4,5 +4,7 @@ class Wood extends Entity {
     function __construct($data) {
         parent::__construct($data);
         $this->count = $data->count; // количество дерева
+        $this->x = $data->x;
+        $this->y = $data->y;
     }
 }
