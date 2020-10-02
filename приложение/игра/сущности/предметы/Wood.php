e0ae9d304afa7cdd58461ccd6822b89db9e5fc4e
Skovorodka-chan <67179020+Skovorodka-chan@users.noreply.github.com>
2020-10-02 19:44:27 +0400

    Update and rename application/game/entities/items/Wood.php to приложение/игра/сущности/предметы/Wood.php

--- приложение/игра/сущности/предметы/Wood.php
+++ приложение/игра/сущности/предметы/Wood.php
@@ -4,7 +4,5 @@ class Wood extends Entity {
     function __construct($data) {
         parent::__construct($data);
         $this->count = $data->count; // количество дерева
-        $this->x = $data->x;
-        $this->y = $data->y;
-    }
+     }
 }
